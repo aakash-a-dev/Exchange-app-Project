a4aa65ceef1955ef1282c3ea4cbdfc000a184bad
Aakash Amod Rajput <aakash.ar1213@gmail.com>
2024-11-22 15:15:13 +0530

    1st phase of UI Done

--- exchange-client/app/page.tsx
+++ exchange-client/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { Markets } from "./Markets";
 
 export default function Home() {
   return (
-   <>
-   Body Here
-   </>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Markets />
+    </main>
   );
 }
