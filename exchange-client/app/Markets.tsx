75a2c31c4ab32546205c74449cdd0cef2526730c
Aakash Amod Rajput <aakash.ar1213@gmail.com>
2024-11-23 19:19:17 +0530

    Recitified syntax issue

--- exchange-client/app/Markets.tsx
+++ exchange-client/app/Markets.tsx
@@ -110,9 +110,9 @@ function MarketHeader() {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 className="lucide lucide-arrow-down h-4 w-4"
               >
                 <path d="M12 5v14"></path>
